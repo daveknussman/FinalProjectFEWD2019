ad787ee0146af412e575fe941e48677313b3be22
daveknussman <dknussman@yahoo.com>
2019-09-05 17:52:27 -0400

    updated js

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,19 @@
+var fn = ' ';
+var ln = ' ';
+var ph = ' ';
+var pt = ' ';
+var em = ' ';
+var ar1 = [fn,ln,ph,pt,em];
+var ar2 = [];
+
+
+
 function newsignup(first,last,phone,phonetype,email) {
-    alert(phonetype);
+    fn = first;
+    ln = last;
+    ph = phone;
+    pt = phonetype;
+    em = email;
+    ar1 = [fn,ln,ph,pt,em];
+    ar2.push(ar1);
 }
